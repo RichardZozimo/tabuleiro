43b23ee20fd17be67cf9c40807224c1bb4651926
Richard Zozimo <richardzozimo@gmail.com>
2022-05-23 20:29:57 -0300

    iniciando projeto tabuleiro para treinamento estilização de componentes

---
+++ components/Linha.jsx
@@ -0,0 +1,20 @@
+﻿import Subdivisao from './Subdivisao';
+import styles from '../styles/Linha.module.css'
+
+export default function Linha(props) {
+  return (
+    <div
+      style={{ flexDirection: props.reverse ? "row-reverse" : "row"}}
+      className={styles.linha}
+    >
+      <Subdivisao />
+      <Subdivisao preta />
+      <Subdivisao />
+      <Subdivisao preta />
+      <Subdivisao />
+      <Subdivisao preta />
+      <Subdivisao />
+      <Subdivisao preta />
+    </div>
+  )
+}
